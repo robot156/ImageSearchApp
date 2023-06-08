fc0097acdcbd65641fef3203191b5c79f6a17b0c
robot156 <gim162913@gmail.com>
2023-06-08 11:23:24 +0900

    이미지검색앱/feature/build-logic 도입 - 디테일 수정

--- build-logic/src/main/java/JvmLibraryPlugin.kt
+++ build-logic/src/main/java/JvmLibraryPlugin.kt
@@ -1,10 +1,6 @@
-import com.imagesearch.convention.ImageSearchConfig
+import com.imagesearch.convention.configureKotlinJvm
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 internal class JvmLibraryPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -13,24 +9,7 @@ internal class JvmLibraryPlugin : Plugin<Project> {
                 apply("org.jetbrains.kotlin.jvm")
             }
 
-            extensions.configure<JavaPluginExtension> {
-                sourceCompatibility = ImageSearchConfig.javaCompileTarget
-                targetCompatibility = ImageSearchConfig.javaCompileTarget
-            }
-
-            tasks.withType<KotlinCompile> {
-                with(kotlinOptions) {
-                    freeCompilerArgs = freeCompilerArgs + listOf(
-                        "-opt-in=kotlin.RequiresOptIn",
-                        // Enable experimental coroutines APIs, including Flow
-                        "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-                        "-opt-in=kotlinx.coroutines.FlowPreview",
-                        "-opt-in=kotlin.Experimental",
-                    )
-
-                    jvmTarget = ImageSearchConfig.javaCompileTarget.toString()
-                }
-            }
+            configureKotlinJvm()
         }
     }
 }
--- build-logic/src/main/java/com/imagesearch/convention/KotlinAndroid.kt
+++ build-logic/src/main/java/com/imagesearch/convention/KotlinAndroid.kt
@@ -3,10 +3,13 @@ package com.imagesearch.convention
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.api.plugins.ExtensionAware
+import org.gradle.api.plugins.JavaPluginExtension
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
+import org.gradle.kotlin.dsl.provideDelegate
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 internal fun Project.configureKotlinAndroid(
     commonExtension: CommonExtension<*, *, *, *>,
@@ -23,19 +26,9 @@ internal fun Project.configureKotlinAndroid(
             targetCompatibility = ImageSearchConfig.javaCompileTarget
             isCoreLibraryDesugaringEnabled = true
         }
-
-        kotlinOptions {
-            freeCompilerArgs = freeCompilerArgs + listOf(
-                "-opt-in=kotlin.RequiresOptIn",
-                // Enable experimental coroutines APIs, including Flow
-                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
-                "-opt-in=kotlinx.coroutines.FlowPreview",
-                "-opt-in=kotlin.Experimental",
-            )
-
-            jvmTarget = ImageSearchConfig.javaCompileTarget.toString()
-        }
     }
+
+    configureKotlin()
 
     val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
 
@@ -44,6 +37,40 @@ internal fun Project.configureKotlinAndroid(
     }
 }
 
-fun CommonExtension<*, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
-    (this as ExtensionAware).extensions.configure("kotlinOptions", block)
+/**
+ * Configure base Kotlin options for JVM (non-Android)
+ */
+internal fun Project.configureKotlinJvm() {
+    extensions.configure<JavaPluginExtension> {
+        // Up to Java 11 APIs are available through desugaring
+        // https://developer.android.com/studio/write/java11-minimal-support-table
+        sourceCompatibility = ImageSearchConfig.javaCompileTarget
+        targetCompatibility = ImageSearchConfig.javaCompileTarget
+    }
+
+    configureKotlin()
+}
+
+/**
+ * Configure base Kotlin options
+ */
+private fun Project.configureKotlin() {
+    // Use withType to workaround https://youtrack.jetbrains.com/issue/KT-55947
+    tasks.withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            // Set JVM target to 11
+            jvmTarget = ImageSearchConfig.javaCompileTarget.toString()
+            // Treat all Kotlin warnings as errors (disabled by default)
+            // Override by setting warningsAsErrors=true in your ~/.gradle/gradle.properties
+            val warningsAsErrors: String? by project
+            allWarningsAsErrors = warningsAsErrors.toBoolean()
+            freeCompilerArgs = freeCompilerArgs + listOf(
+                "-opt-in=kotlin.RequiresOptIn",
+                // Enable experimental coroutines APIs, including Flow
+                "-opt-in=kotlinx.coroutines.ExperimentalCoroutinesApi",
+                "-opt-in=kotlinx.coroutines.FlowPreview",
+                "-opt-in=kotlin.Experimental",
+            )
+        }
+    }
 }
